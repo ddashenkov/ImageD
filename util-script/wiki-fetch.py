eec1618a46b8695d1deed22de88983222b616a71
Dmytro Dashenkov <ymhik98@gmail.com>
2023-02-21 18:58:49 +0200

    Add missing label descriptiuons

--- util-script/wiki-fetch.py
+++ util-script/wiki-fetch.py
@@ -20,47 +20,72 @@ def prepare_definition(wiki_article: str) -> str:
     return ' '.join(words)
 
 
-print(f'Reading terms from `{source_file_name}`.')
-with open(source_file_name) as file:
-    reader = csv.reader(file)
-    next(reader)
-    spreadsheet = [row for row in reader]
+def fetch_from_wiki():
+    print(f'Reading terms from `{source_file_name}`.')
+    with open(source_file_name) as file:
+        reader = csv.reader(file)
+        next(reader)
+        spreadsheet = [row for row in reader]
 
-errors = []
-ERROR_NOT_FOUND = 0
-ERROR_OTHER = 1
-ERROR_NETWORK = 2
+    errors = []
+    ERROR_NOT_FOUND = 0
+    ERROR_OTHER = 1
+    ERROR_NETWORK = 2
 
-for row in tqdm.tqdm(spreadsheet, 'Fetching Wiki articles...', leave=True):
-    identifier = row[0]
-    term = row[1]
-    try:
-        wiki_article = wikipedia.page(term)
-        article = wiki_article.content
-        definitions.append([identifier, prepare_definition(article)])
-    except wikipedia.PageError as e:
-        print(f'No Wiki page found for term {identifier}({term}).')
-        print('\n')
-        errors.append([identifier, term, ERROR_NOT_FOUND])
-    except wikipedia.WikipediaException as e:
-        print(f'Failed to fetch a Wikipedia article for {identifier}({term}): ' + str(e))
-        print('\n')
-        errors.append([identifier, term, ERROR_OTHER])
-    except Exception as e:
-        print(f'Failed to fetch a Wikipedia article for {identifier}({term}): ' + str(e))
-        print('\n')
-        errors.append([identifier, term, ERROR_NETWORK])
+    for row in tqdm.tqdm(spreadsheet, 'Fetching Wiki articles...', leave=True):
+        identifier = row[0]
+        term = row[1]
+        try:
+            wiki_article = wikipedia.page(term)
+            article = wiki_article.content
+            definitions.append([identifier, prepare_definition(article)])
+        except wikipedia.PageError as e:
+            print(f'No Wiki page found for term {identifier}({term}).')
+            print('\n')
+            errors.append([identifier, term, ERROR_NOT_FOUND])
+        except wikipedia.WikipediaException as e:
+            print(f'Failed to fetch a Wikipedia article for {identifier}({term}): ' + str(e))
+            print('\n')
+            errors.append([identifier, term, ERROR_OTHER])
+        except Exception as e:
+            print(f'Failed to fetch a Wikipedia article for {identifier}({term}): ' + str(e))
+            print('\n')
+            errors.append([identifier, term, ERROR_NETWORK])
 
-print(f'Writing definitions into `{target_file_name}`')
-with open(target_file_name, 'w') as file:
-    writer = csv.writer(file)
-    writer.writerow(['LabelName', 'Definition'])
-    writer.writerows(definitions)
+    print(f'Writing definitions into `{target_file_name}`')
+    with open(target_file_name, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(['LabelName', 'Definition'])
+        writer.writerows(definitions)
 
-print(f'Writing errors into `{target_file_name}.error.csv`')
-with open(target_file_name + '.error.csv', 'w') as file:
-    writer = csv.writer(file)
-    writer.writerow(['LabelName', 'DisplayName', 'ErrorType'])
-    writer.writerows(errors)
+    print(f'Writing errors into `{target_file_name}.error.csv`')
+    with open(target_file_name + '.error.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(['LabelName', 'DisplayName', 'ErrorType'])
+        writer.writerows(errors)
 
-print('Done.')
+    print('Done.')
+
+
+def prepare_manual():
+    file_name = sys.argv[2]
+    with open(file_name) as file:
+        reader = csv.reader(file)
+        next(reader)
+        rows = [r for r in reader]
+    for i in range(len(rows)):
+        rows[i] = [rows[i][0], rows[i][1]]
+        rows[i][1] = prepare_definition(rows[i][1])
+    with open(file_name, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(['LabelName', 'Definition'])
+        writer.writerows(rows)
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == '--prepare':
+        print('Manual prepare mode')
+        prepare_manual()
+    else:
+        print('Auto fetch mode')
+        fetch_from_wiki()
